14c0f9392f529cb66fbd6b17e4804c427e0a0c5b
dyegot2005 <dyegot2005@gmail.com>
2025-06-13 03:20:26 +0000

    finalized

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const quackSound = document.getElementById('quack');
+const meowSound = document.getElementById('meow');
 
 function quack(){
         quackSound.play();
+}
+
+function meow(){
+        meowSound.play();
 }
